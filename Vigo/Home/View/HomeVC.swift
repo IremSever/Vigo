7d5eeb2da5846765d708f97eebfb7df05a8a25ab
İrem Sever <iremsever_@outlook.com>
2025-02-10 07:02:00 +0300

    Update

--- Vigo/Home/View/HomeVC.swift
+++ Vigo/Home/View/HomeVC.swift
@@ -3,14 +3,13 @@
 //  Vigo
 //
 //  Created by İrem Sever on 31.01.2025.
-//
 import SwiftUI
 
 struct HomeVC: View {
     @StateObject private var viewModel = HomeViewModel()
-  
     @State var scrollOffsetY: CGFloat = 0
     @State var scrollProgressX: CGFloat = 0
+
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
@@ -18,35 +17,17 @@ struct HomeVC: View {
                     LazyVStack(alignment: .leading, spacing: 2) {
                         InfoTop()
                             .zIndex(1)
+                        
                         if let homeData = viewModel.homeModel?.data {
-                            
                             ForEach(homeData.indices, id: \.self) { index in
-                                let section = homeData[index]
-
-                                if let widgetTitle = section.config.widgetTitle?.text {
+                                if let widgetTitle = viewModel.homeModel?.data[index].config.widgetTitle?.text, !widgetTitle.isEmpty {
                                     Text(widgetTitle)
                                         .font(.headline)
                                         .foregroundColor(.white)
                                         .padding(.horizontal)
                                 }
-
-                                switch section.config.widget.template {
-                                case "headlines":
-                                    AlbumCell(viewModel: viewModel, widgetTitle: section.config.widgetTitle?.text ?? "")
-
-                                case "broadcast":
-                                    StreamCell(viewModel: viewModel)
-                                case "cardFullImage1":
-                                    SeriesCell(viewModel: viewModel, widgetTitle: section.config.widgetTitle?.text ?? "")
-                                case "cardFullImage3":
-                                    CardCell(viewModel: viewModel, widgetTitle: section.config.widgetTitle?.text ?? "")
-                                case "cardTopImage1":
-                                    CardCell(viewModel: viewModel, widgetTitle: section.config.widgetTitle?.text ?? "")
-                                        .padding(.bottom, 50)
-                                default:
-                                    SeriesCell(viewModel: viewModel, widgetTitle: section.config.widgetTitle?.text ?? "")
-                                        
-                                }
+                                
+                                RowView(viewModel: viewModel, index: index)
                             }
                         }
                     }
@@ -58,39 +39,50 @@ struct HomeVC: View {
                         .scaleEffect(y: -1)
                         .ignoresSafeArea(edges: .all)
                 }
-                
-         
             }
         }
         .onAppear {
             viewModel.fetchHomeData{}
         }
     }
-    
-   
-    
 }
-struct DynamicCell: View {
-    @ObservedObject var viewModel: HomeViewModel
+
+struct RowView: View {
+    let viewModel: HomeViewModel
+    let index: Int
 
     var body: some View {
-        Group {
-            if let homeData = viewModel.homeModel?.data {
-                let newsItems = homeData.flatMap { $0.news ?? [] }
-
-                switch (newsItems.first(where: { $0.external.starts(with: "video://") }), newsItems.first(where: { $0.external.starts(with: "external://https:") })) {
-                case (nil, let nonVideoItem?):
-                    AchiveCell(viewModel: viewModel)
-                        .zIndex(-1)
-                case (let videoItem?, _):
-                    AlbumCell(viewModel: viewModel)
-                        .zIndex(-1)
+        if let template = viewModel.homeModel?.data[index].config.widget.template {
+            switch template {
+            case "headlines":
+                let newsItems = viewModel.homeModel?.data[index].news ?? []
                 
-                default:
-                    EmptyView() // veya başka bir default view
+                let externalNewsItems = newsItems.filter { newsItem in
+                    newsItem.external.starts(with: "external://https:")
                 }
+                let videoNewsItems = newsItems.filter { newsItem in
+                    newsItem.external.starts(with: "video://")
+                }
+                if !externalNewsItems.isEmpty {
+                    AchiveCell(viewModel: viewModel)
+                } else if !videoNewsItems.isEmpty {
+                    AlbumCell(viewModel: viewModel)
+                } else {
+                    Text("No news available")
+                        .foregroundColor(.white)
+                }
+            case "broadcast":
+                StreamCell(viewModel: viewModel)
+
+            case "cardFullImage1":
+                SeriesCell(viewModel: viewModel, widgetTitle: viewModel.homeModel?.data[index].config.widgetTitle?.text ?? "")
+
+            case "cardFullImage3", "cardTopImage1":
+                CardCell(viewModel: viewModel, widgetTitle: viewModel.homeModel?.data[index].config.widgetTitle?.text ?? "")
+
+            default:
+                SeriesCell(viewModel: viewModel, widgetTitle: viewModel.homeModel?.data[index].config.widgetTitle?.text ?? "")
             }
         }
-        .onAppear()
     }
 }
