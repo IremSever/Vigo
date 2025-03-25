20ba1789c9253add6572bc50b26c46a9dba7f944
İrem Sever <iremsever_@outlook.com>
2025-03-25 07:07:16 +0300

    update

---
+++ Vigo/Cells/Common/StarRatingView.swift
@@ -0,0 +1,43 @@
+//
+//  StarRatingView.swift
+//  Vigo
+//
+//  Created by İrem Sever on 20.03.2025.
+//
+
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct StarRatingView: View {
+    var rating: Int
+    var maxRating: Int = 5
+
+    @State private var animatedRating: Int = 0
+
+    var body: some View {
+        VStack(spacing: 5) {
+            ForEach(0..<maxRating, id: \.self) { index in
+                SwiftUI.Image(systemName: index < animatedRating ? "star.fill" : "star")
+                    .foregroundColor(.orange.opacity(1))
+                    .font(.system(size: 20))
+                    .scaleEffect(index < animatedRating ? 1.2 : 1.0)
+                    .animation(.easeInOut(duration: 0.8), value: animatedRating)
+                    .shadow(color: .purple.opacity(0.35), radius: 5)
+            }
+        }
+        .onAppear {
+            animateStars()
+        }
+    }
+
+    private func animateStars() {
+        for i in 0...rating {
+            DispatchQueue.main.asyncAfter(deadline: .now() + (0.2 * Double(i))) {
+                withAnimation {
+                    animatedRating = i
+                }
+            }
+        }
+    }
+}
--- Vigo/Favorites/FavoritesView.swift
+++ Vigo/Favorites/FavoritesView.swift
@@ -19,6 +19,55 @@ struct FavoritesView: View {
                     UserCard()
                     ScrollView  {
                         VStack(alignment: .leading, spacing: 20) {
+                            Text("Rated")
+                                .foregroundColor(.white)
+                                .font(.exoBold(size: 18))
+                                .padding(.horizontal, 16)
+                            
+                            VStack {
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 12) {
+                                        ForEach(viewModel.ratings.keys.sorted(), id: \.self) { key in
+                                            if let item = viewModel.favoriteItems.first(where: { $0.title == key }),
+                                               let imageUrl = URL(string: item.image ?? ""),
+                                               let seriesName = item.title,
+                                               let rating = viewModel.ratings[key] {
+                                                
+                                                VStack {
+                                                    ZStack {
+                                                     
+                                                        WebImage(url: imageUrl)
+                                                            .resizable()
+                                                            .aspectRatio(contentMode: .fill)
+                                                            .frame(width: 140, height: 200)
+                                                            .cornerRadius(20)
+                                                            .shadow(color: .purple.opacity(0.35), radius: 5)
+                                                            .overlay(
+                                                                LinearGradient(
+                                                                    gradient: Gradient(colors: [Color.black.opacity(0.5), Color.clear]),
+                                                                    startPoint: .bottom,
+                                                                    endPoint: UnitPoint(x: 0.2, y: 0.2)
+                                                                )
+                                                                .cornerRadius(20)
+                                                            )
+                                                        StarRatingView(rating: rating).padding(.horizontal, 16)
+                                                            .position(x: 140, y: 100)
+                                                       
+                                                    }
+//                                                    Text(seriesName)
+//                                                        .foregroundColor(.orange)
+//                                                        .font(.exoSemiBold(size: 14))
+//                                                        .lineLimit(1)
+//                                                        .frame(width: 120)
+//                                                        .padding(.bottom, 5)
+//                                                    
+                                                    
+                                                }
+                                            }
+                                        }
+                                    } .padding(.horizontal, 16)
+                                }
+                            }
                             Text("Favourites")
                                 .foregroundColor(.white)
                                 .font(.exoBold(size: 18))
@@ -67,72 +116,21 @@ struct FavoritesView: View {
                                     }
                                 } .padding(.horizontal, 16)
                             }
+                          
+                       
+                            .padding(.bottom, 70)
                             
-                            
-                            Text("Rated")
-                                .foregroundColor(.white)
-                                .font(.exoBold(size: 18))
-                                .padding(.horizontal, 16)
-                            
-                    
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                HStack(spacing: 12) {
-                                    ForEach(viewModel.ratings.keys.sorted(), id: \ .self) { key in
-                                        if let item = viewModel.favoriteItems.first(where: { $0.title == key }), let imageUrl = URL(string: item.image ?? ""),   let seriesName = item.title {
-                                            VStack {
-                                                WebImage(url: imageUrl)
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fill)
-                                                    .frame(width: 120, height: 120)
-                                                    .cornerRadius(20)
-                                              
-//                                              if let rating = viewModel.ratings[key] {
-//                                                    VStack (alignment: .center){
-////                                                        Text("\(rating)/5")
-//                                                        Text("\(rating)")
-//                                                            .foregroundColor(.orange)
-//                                                            .font(.exoBold(size: 20))
-////                                                            .rotationEffect(.degrees(270))
-////                                                            .position(x: 95, y: 50)
-//                                                    }
-//                                                }
-                                              
-                                                Text(seriesName)
-                                                    .foregroundColor(.purple)
-                                                    .font(.exoMedium(size: 14))
-                                                    .lineLimit(1)
-                                                    .frame(width: 120)
-                                                    .padding(.bottom, 5)
-
-                                                
-                                                if let rating = viewModel.ratings[key] {
-                                                    VStack(alignment: .center) {
-                                                        HStack(spacing: 2) {
-                                                            ForEach(0..<rating, id: \.self) { _ in
-                                                                SwiftUI.Image(systemName: "star.fill")
-                                                                    .foregroundColor(.purple)
-                                                                    .font(.system(size: 10))
-                                                            }
-                                                        }
-                                                    }
-                                                }
-
-                                              
-                                            }
-                                        }
-                                    }
-                                }
-                                .padding(.horizontal, 16)
-                            }.padding(.bottom, 70)
                         }
-                      
+                        
                     }
                     .onAppear {
                         viewModel.loadFavorites()
                         viewModel.loadRatings()
                         viewModel.loadDownload()
                     }
-                }}}
+                }
+            }
+        }
         .background {
             Rectangle()
                 .fill(.black.gradient)
